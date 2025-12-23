aa7373db036a6080795197634e624ba71df5df2a
ateeg <ateeg1999@gmail.com>
2025-12-23 17:20:29 +0300

    feat: establish core package entry point exporting modules and version constant

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,35 +1,16 @@
 export const VERSION = "0.1.0";
 
 // Core interfaces and types
-export {
-    Document,
-    DocumentChunk,
-    RetrievedDocument,
-    RAGContext,
-    RAGResponse,
-    LLMResponse,
-    ChatMessage,
-    IVectorStore,
-    ILLMProvider,
-    IEmbeddingProvider,
-    IMemoryManager,
-    IContextBuilder,
-    IReranker,
-    IRAGEngine,
-    ChunkingStrategy,
-    IDocumentProcessor,
-    IDocumentLoader
-} from "./interfaces";
+export * from "./interfaces";
 
 // Document processor
-export { DocumentProcessor } from "./document-processor";
-export type { ChunkingOptions } from "./document-processor";
+export * from "./document-processor";
 
 // Retriever
-export { Retriever } from "./retriever";
+export * from "./retriever";
 
 // Context builder
-export { ContextBuilder } from "./context-builder";
+export * from "./context-builder";
 
 // RAG engine
-export { RAGEngine } from "./rag-engine";
+export * from "./rag-engine";
